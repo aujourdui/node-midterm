2c32d9c00cb7c300c5a78a3212f7c7dfe6713dc1
ito <kyosuke.ito@gmail.com>
2022-05-19 13:10:31 -0700

    create basic crud function

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const db = require("../services/dbsqlite");
 
 router.get("/", (req, res) => {
-  const sql = "SELECT * FROM Todos ORDER BY Title";
+  const sql = "SELECT * FROM Blog ORDER BY Title";
   db.all(sql, [], (err, rows) => {
     if (err) return console.error(err.message);
 
@@ -14,10 +14,10 @@ router.get("/create", (req, res) => {
   res.render("create", { model: {} });
 });
 router.post("/create", (req, res) => {
-  const sql = "INSERT INTO Todos (Title) VALUES (?)";
-  const todo = [req.body.Title];
+  const sql = "INSERT INTO Blog (Title) VALUES (?)";
+  const blog = [req.body.Title];
 
-  db.run(sql, todo, (err) => {
+  db.run(sql, blog, (err) => {
     if (err) return console.error(err.message);
 
     res.redirect("/");
@@ -25,24 +25,24 @@ router.post("/create", (req, res) => {
 });
 router.get("/edit", (req, res) => {
   const id = req.query.id;
-  const sql = "select * from Todos where id = ?";
+  const sql = "select * from Blog where id = ?";
   db.get(sql, [id], (err, row) => {
     if (err) return console.error(err.message);
     res.render("edit", { model: row });
   });
 });
 router.post("/edit", (req, res) => {
-  const sql = "update Todos set Title = ? where id = ?";
-  const todo = [req.body.Title];
+  const sql = "update Blog set Title = ? where id = ?";
+  const blog = [req.body.Title];
   const id = req.body.id;
   console.log(id);
 
-  db.run(sql, [todo, id]);
+  db.run(sql, [blog, id]);
   res.redirect("/");
 });
 router.get("/delete", (req, res) => {
   const id = req.query.id;
-  const sql = "select * from Todos where id = ?";
+  const sql = "select * from Blog where id = ?";
   db.get(sql, [id], (err, row) => {
     if (err) return console.error(err.message);
     res.render("delete", { model: row });
@@ -50,7 +50,7 @@ router.get("/delete", (req, res) => {
 });
 router.post("/delete", (req, res) => {
   const id = req.body.id;
-  const sql = "delete from Todos where id = ?";
+  const sql = "delete from Blog where id = ?";
   db.run(sql, id);
   res.redirect("/");
 });
--- src/services/dbsqlite.js
+++ src/services/dbsqlite.js
@@ -1,30 +1,29 @@
-const sqlite3 = require('sqlite3')
-const path = require('path')
+const sqlite3 = require("sqlite3");
+const path = require("path");
 
-const db_name = path.join(__dirname, '..', 'data', 'todo.db')
-const db = new sqlite3.Database(db_name, (err) =>{
-    if(err) return console.error(err.message)
-    console.log('Successfully connected to the database')
-})
+const db_name = path.join(__dirname, "..", "data", "blog.db");
+const db = new sqlite3.Database(db_name, (err) => {
+  if (err) return console.error(err.message);
+  console.log("Successfully connected to the database");
+});
 
-const sql_create = `CREATE TABLE IF NOT EXISTS Todos (
+const sql_create = `CREATE TABLE IF NOT EXISTS Blog (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     Title VARCHAR(100) NOT NULL 
-);`
+);`;
 
 db.run(sql_create, (err) => {
-    if(err) return console.error(err.message)
+  if (err) return console.error(err.message);
 
-    const sql_insert = `INSERT INTO Todos (ID, Title) VALUES
-    (1, 'Buy Cake'),
-    (2, 'Sleep');`
+  const sql_insert = `INSERT INTO Blog (ID, Title) VALUES
+    (1, 'Article1'),
+    (2, 'Article2');`;
 
-    db.run(sql_insert, (err) => {
-        if(err) return console.error(err.message)
+  db.run(sql_insert, (err) => {
+    if (err) return console.error(err.message);
 
-        console.log('Successfully created Todos table')
-    })
-    
-})
+    console.log("Successfully created Blog table");
+  });
+});
 
-module.exports = db
+module.exports = db;
